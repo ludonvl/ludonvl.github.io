2f1430327376045018afbfc7d0775c6d63e67121
Ludovic Nouvel <nouvel.l@live.fr>
2015-11-05 21:40:18 +0100

    contact form js + html

--- js/contact_me.js
+++ js/contact_me.js
@@ -1,100 +1,38 @@
 $(function() {
-    $("#button-send-form").click(function(e) {
-        $("#contactForm").submit();
-    });
-
     $("#contactForm").submit(function(e) {
         e.preventDefault();
+        $.ajax({
+            url: "//formspree.io/ludovic.nouvel@epitech.eu", 
+            method: "POST",
+            data: $(this).serialize(),
+            dataType: "json",
+            success: function(data){
+                // Enable button & show success message
+                $("#btnSubmit").attr("disabled", false);
+                $('#success').html("<div class='alert alert-success'>");
+                $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                    .append("</button>");
+                $('#success > .alert-success')
+                    .append("<strong>Votre message a été envoyé avec succes. </strong>");
+                $('#success > .alert-success')
+                    .append('</div>');
 
-            $.ajax({
-                url: "//formspree.io/ludovic.nouvel@epitech.eu",
-                type: "POST",
-                data: {message: "hello!"},
-                dataType: "json",
-                success: function(data) {
-                    // Enable button & show success message
-                    $("#btnSubmit").attr("disabled", false);
-                    $('#success').html("<div class='alert alert-success'>");
-                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-success')
-                        .append("<strong>Votre message a été envoyé avec succes. </strong>");
-                    $('#success > .alert-success')
-                        .append('</div>');
-
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
-                },
-                error: function() {
-                    // Fail message
-                    $('#success').html("<div class='alert alert-danger'>");
-                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Désolé " + firstName + ", il semblerai que mon serveur mail ne répond pas. Essaie plus tard !");
-                    $('#success > .alert-danger').append('</div>');
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
-                },
-            });
-    });
-
-/*
-    $("input,textarea").jqBootstrapValidation({
-        preventSubmit: true,
-        submitError: function($form, event, errors) {
-            // additional error messages or events
-        },
-        submitSuccess: function($form, event) {
-            // Prevent spam click and default submit behaviour
-            $("#btnSubmit").attr("disabled", true);
-            event.preventDefault();
-            
-            // get values from FORM
-            var name = $("input#name").val();
-            var email = $("input#email").val();
-            var phone = $("input#phone").val();
-            var message = $("textarea#message").val();
-            var firstName = name; // For Success/Failure Message
-            // Check for white space in name for Success/Fail message
-            if (firstName.indexOf(' ') >= 0) {
-                firstName = name.split(' ').slice(0, -1).join(' ');
+                //clear all fields
+                $('#contactForm').trigger("reset");
+            },
+            error: function(err) {
+                // Fail message
+                $('#success').html("<div class='alert alert-danger'>");
+                $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                    .append("</button>");
+                $('#success > .alert-danger').append("<strong>Désolé, il semblerai que mon serveur mail ne répond pas. Essaie plus tard !");
+                $('#success > .alert-danger').append('</div>');
+                //clear all fields
+                $('#contactForm').trigger("reset");
             }
-            $.ajax({
-                url: "//formspree.io/nouvel.l@live.fr",
-                type: "POST",
-                data: {message: "hello!"},
-                dataType: "json",
-                success: function(data) {
-                    // Enable button & show success message
-                    $("#btnSubmit").attr("disabled", false);
-                    $('#success').html("<div class='alert alert-success'>");
-                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-success')
-                        .append("<strong>Votre message a été envoyé avec succes. </strong>");
-                    $('#success > .alert-success')
-                        .append('</div>');
-
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
-                },
-                error: function() {
-                    // Fail message
-                    $('#success').html("<div class='alert alert-danger'>");
-                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Désolé " + firstName + ", il semblerai que mon serveur mail ne répond pas. Essaie plus tard !");
-                    $('#success > .alert-danger').append('</div>');
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
-                },
-            })
-        },
-        filter: function() {
-            return $(this).is(":visible");
-        },
+        });
     });
-*/
+
     $("a[data-toggle=\"tab\"]").click(function(e) {
         e.preventDefault();
         $(this).tab("show");
